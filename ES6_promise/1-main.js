ee50d48c82fd91fee462097fb5134c6307dd2b72
mateomema <mateomema1@gmail.com.com>
2024-07-16 10:41:08 +0200

    tasks

---
+++ ES6_promise/1-main.js
@@ -0,0 +1,4 @@
+import getfullResponseFromAPI from './1-promise';
+
+console.log(getfullResponseFromAPI(true));
+console.log(getfullResponseFromAPI(false));
---
+++ ES6_promise/2-main.js
@@ -0,0 +1,4 @@
+import handleResponseFromAPIfrom "./2-then";
+
+const promise = Promise.resolve();
+handleResponseFromAPIfrom(promise);
