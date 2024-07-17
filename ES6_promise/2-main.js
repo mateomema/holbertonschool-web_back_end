8abb0b5dd6ba4e2b547bd145a9c347bd20167e29
mateomema <mateomema1@gmail.com.com>
2024-07-17 16:36:50 +0200

    update

--- ES6_promise/2-main.js
+++ ES6_promise/2-main.js
@@ -1,4 +1,17 @@
 import handleResponseFromAPI from "./2-then";
 
 const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+handleResponseFromAPI(promise);export function uploadPhoto() {
+    return Promise.resolve({
+      status: 200,
+      body: 'photo-profile-1',
+    });
+  }
+  
+  
+  export function createUser() {
+    return Promise.resolve({
+      firstName: 'Guillaume',
+      lastName: 'Salva',
+    });
+  }
