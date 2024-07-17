06824c3e5dec0ea96eecd5e80395a6339f30b919
mateomema <mateomema1@gmail.com.com>
2024-07-17 16:02:24 +0200

    ksk

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,13 @@
-function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      return new Error();
-    });
-}
+export default function handleResponseFromAPI(promise) {
 
-export default handleResponseFromAPI;
+	return promise
+		.then(() => {
+			return { status: 200, body: "success" };
+		})
+		.catch((err) => {
+			return new Error();
+		})
+		.finally(() => {
+			console.log("Got a response from the API");
+		});
+}
